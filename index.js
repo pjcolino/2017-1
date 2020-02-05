788fe4ee7162a7f1be32cf26018b0b11c6af0c24
pjcolino <pjcolino@seattleacademy.org>
2020-02-05 09:33:20 -0800

    Add server pages

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express()
 const port = 1513
 let counter = 0
 
+app.use (express.static('public'))
+
 app.get('/', (req, res) => res.send('Hello PJ!'))
 
 app.get('/counter', function (req, res) {
@@ -10,6 +12,16 @@ app.get('/counter', function (req, res) {
 	counter = counter + 1;
 
   res.send('counter ' + counter);
+})
+
+app.get('/date', function (req, res) {
+
+	let d = new Date();
+	console.log(d);
+
+	counter = counter + 1;
+
+  res.send('Date ' + d);
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
