f51f36fbf9aadd7454eac2e4d222cb332b9b9733
pjcolino <pjcolino@seattleacademy.org>
2020-02-05 09:14:50 -0800

    Ass counter to server

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-console.log("hello world!"):.=
+const express = require('express')
+const app = express()
+const port = 1513
+let counter = 0
+
+app.get('/', (req, res) => res.send('Hello PJ!'))
+
+app.get('/counter', function (req, res) {
+
+	counter = counter + 1;
+
+  res.send('counter ' + counter);
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
